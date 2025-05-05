409d7e7672d17dc7be3d2495edd733a33d5da82e
Alex <xelastlaurent@gmail.com>
2025-05-05 13:02:24 -0300

    Capitalization matters

--- platform.py
+++ platform.py
@@ -23,7 +23,7 @@ from platformio.util import get_systype
 IS_WINDOWS = sys.platform.startswith("win")
 
 
-class TeensySDRAMPlatform(PlatformBase):
+class TeensysdramPlatform(PlatformBase):
 
     @staticmethod
     def _is_macos_x86():
